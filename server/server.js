352f2bfc5a4d844d5fda80b090a6baf39023c7c6
Evon Bausa <evon.bausa@gmail.com>
2022-11-20 19:17:59 -0800

    Append dotenv dependency

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
+/**
+ * Require node dependencies.
+ */
 const express = require("express");
+/* End of node dependencies. */
+
+require('dotenv').config();
 
 const app = express(); // Initiate express.
+const port = process.env.PORT || 8000;
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+    console.log(`Server running on port ${port}`);
 })
