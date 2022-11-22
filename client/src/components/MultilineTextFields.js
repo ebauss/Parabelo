d2c88b3f9ea7b6944a4a8219006c83a77d976eac
Evon Bausa <evon.bausa@gmail.com>
2022-11-22 15:08:23 -0800

    Append multiline text field

---
+++ client/src/components/MultilineTextFields.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function MultilineTextFields() {
+  const [value, setValue] = React.useState('Controlled');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          fullWidth
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+      </div>
+    </Box>
+  );
+}
