c5aa368c5887d3c79cd45a8a67cfa6dd35ec1595
Evon Bausa <evon.bausa@gmail.com>
2022-11-22 16:22:54 -0800

    Append basic prototype

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -5,7 +5,15 @@ import Stack from '@mui/material/Stack';
 export default function TextButtons() {
     return (
         <Stack spacing={2} direction="row">
-            <Button variant="outlined">Outlined</Button>
+            <Button variant="outlined" onClick={testPrompt}>Test Prompt</Button>
         </Stack>
     );
 }
+
+function testPrompt() {
+    console.log('fetch is about to be called');
+    fetch('http://localhost:8000/test');
+    console.log('fetch has been called');
+}
+
+
--- server/server.js
+++ server/server.js
@@ -2,13 +2,36 @@
  * Require node dependencies.
  */
 const express = require("express");
-/* End of node dependencies. */
+/* ------------------------------------ */
 
 require('dotenv').config();
 
 const app = express(); // Initiate express.
 const port = process.env.PORT || 8000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+})
+
+app.get('/test', async (req, res) => {
+    const { Configuration, OpenAIApi } = require("openai");
+    const somePrompt = "Write a tagline for an artificial intelligence text generator";
+
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: somePrompt,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+    console.log(`Prompt = ${somePrompt}`);
+    console.log(`Result: ${response.data.choices[0].text}`);
 })
