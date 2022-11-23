d8c2938a7ff44b349d2e73368f5fc276d2b4a123
Evon Bausa <evon.bausa@gmail.com>
2022-11-22 23:47:56 -0800

    Revise MultilineTextFields component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import TextButtons from './components/button';
+import MultilineTextFields from "./components/MultilineTextFields";
 
 function App() {
-  return (
-    <div className="App">
-      <TextButtons></TextButtons>
-    </div>
-  );
+    return (
+        <div className="App">
+            <TextButtons></TextButtons>
+            <MultilineTextFields></MultilineTextFields>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/MultilineTextFields.js
+++ client/src/components/MultilineTextFields.js
@@ -7,27 +7,29 @@ export default function MultilineTextFields() {
 
   const handleChange = (event) => {
     setValue(event.target.value);
+    console.log(value);
   };
 
   return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          fullWidth
-          id="outlined-multiline-static"
-          label="Multiline"
-          multiline
-          rows={4}
-          defaultValue="Default Value"
-        />
-      </div>
-    </Box>
+      <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': {m: 1, width: '25ch'},
+          }}
+          noValidate
+          autoComplete="off"
+      >
+        <div>
+          <TextField
+              fullWidth
+              onChange={handleChange}
+              id="outlined-multiline-static"
+              label="Multiline"
+              multiline
+              rows={4}
+              defaultValue="Default Value"
+          />
+        </div>
+      </Box>
   );
 }
