2a01ed62dcd2c08e5c1432d5a4749b60be83d165
Evon Bausa <evon.bausa@gmail.com>
2022-11-23 23:23:23 -0800

    Append empty ResultTextComponent

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import InputTextComponent from "./components/InputTextComponent/InputTextComponent";
+import ResultTextComponent from "./components/ResultTextComponent/ResultTextComponent";
 
 function App() {
     return (
         <div className="App">
             <InputTextComponent></InputTextComponent>
+            <ResultTextComponent></ResultTextComponent>
         </div>
     );
 }
---
+++ client/src/components/ResultTextComponent/ResultTextComponent.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+
+export default function ResultTextComponent() {
+    return (
+        <div>
+
+        </div>
+    )
+}
