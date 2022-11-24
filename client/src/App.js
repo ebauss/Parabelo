1317aa22eed821c2558967c8c313133512aaee2a
Evon Bausa <evon.bausa@gmail.com>
2022-11-23 20:07:44 -0800

    Delete not needed files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import TextButtons from './components/button';
-import MultilineTextFields from "./components/MultilineTextFields";
 
 function App() {
     return (
