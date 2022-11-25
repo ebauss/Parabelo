abee091fa8fc841a2396512c38f73fbb50807015
Evon Bausa <evon.bausa@gmail.com>
2022-11-24 22:24:04 -0800

    Revise /test route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
  */
 const express = require("express");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 /* ------------------------------------ */
 
 require('dotenv').config();
@@ -10,15 +11,22 @@ require('dotenv').config();
 const app = express(); // Initiate express.
 const port = process.env.PORT || 8000;
 
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}));
+
 app.use(bodyParser.json());
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
 
-app.get('/test', async (req, res) => {
+app.post('/test', async (req, res) => {
+    console.log('Ai is generating the blog post. Please wait...')
+
     const { Configuration, OpenAIApi } = require("openai");
-    const somePrompt = "Write a tagline for an artificial intelligence text generator";
+    const somePrompt = req.body.prompt;
 
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
@@ -28,8 +36,8 @@ app.get('/test', async (req, res) => {
     const response = await openai.createCompletion({
         model: "text-davinci-002",
         prompt: somePrompt,
-        temperature: 0.7,
-        max_tokens: 256,
+        temperature: 0.9,
+        max_tokens: 3000,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
@@ -37,4 +45,6 @@ app.get('/test', async (req, res) => {
 
     console.log(`Prompt = ${somePrompt}`);
     console.log(`Result: ${response.data.choices[0].text}`);
+
+    res.send(response.data.choices[0].text);
 })
