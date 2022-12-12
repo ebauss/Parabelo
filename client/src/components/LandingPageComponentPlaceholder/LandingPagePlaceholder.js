6b0e5385b5cea365d383ec0e8704ea122e3479ae
Evon Bausa <evon.bausa@gmail.com>
2022-12-12 10:48:32 -0800

    Append button that goes to landing page from web app

--- client/src/components/LandingPageComponentPlaceholder/LandingPagePlaceholder.js
+++ client/src/components/LandingPageComponentPlaceholder/LandingPagePlaceholder.js
@@ -1,7 +1,21 @@
 import * as React from 'react';
+import {Button} from "@mui/material";
 
 export default function LandingPagePlaceholder() {
+    /**
+     * Handles the go to web app click from the button.
+     */
+    const handleGoToWebAppClick = () => {
+        // Try using window.open() first. Maybe switch to Link later on?
+        window.open("/app", "_self");
+    }
+
     return (
-        <h1>Landing Page</h1>
+        <div>
+            <h1>Landing Page Placeholder</h1>
+            <Button variant="outlined" size="large" onClick={handleGoToWebAppClick}>
+                Large
+            </Button>
+        </div>
     );
 }
