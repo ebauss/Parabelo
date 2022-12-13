c6ef6cc84a5ed48d07baae4235c30f0dfd34eec3
Evon Bausa <evon.bausa@gmail.com>
2022-12-12 17:03:34 -0800

    Append WebApplication.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import BlogPost from "./pages/WebApplication/BlogPost/BlogPost";
 import LandingPagePlaceholder from "./components/LandingPageComponentPlaceholder/LandingPagePlaceholder";
 import Paraphrasing from "./pages/WebApplication/Paraphrasing/Paraphrasing";
+import WebApplication from "./pages/WebApplication/WebApplication";
 import {
     Routes,
     Route
 } from "react-router-dom";
 
 
+
 function App() {
     return (
         <div className="App">
             <Routes>
+                <Route path="/app" element={<WebApplication/>}/>
                 <Route path="/blogpost" element={<BlogPost/>}/>
                 <Route path="/paraphrasing" element={<Paraphrasing/>}/>
                 <Route path="/" element={<LandingPagePlaceholder/>}/>
---
+++ client/src/pages/WebApplication/WebApplication.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function WebApplication() {
+    return (
+        <div>
+            <h1>Web Application</h1>
+        </div>
+    )
+}
