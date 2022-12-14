639f3754c76bb68fa179735feb8373ab68af8fcf
Evon Bausa <evon.bausa@gmail.com>
2022-12-13 21:47:16 -0800

    Rename component in the function

--- client/src/components/NavbarWebApp/NavbarWebApp.js
+++ client/src/components/NavbarWebApp/NavbarWebApp.js
@@ -89,7 +89,7 @@ const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})
     }),
 );
 
-export default function MiniDrawer() {
+export default function NavbarWebApp() {
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
 
