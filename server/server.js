1cbf23aab01af8974d60ade5cf326b29d26f92d4
Evon Bausa <evon.bausa@gmail.com>
2023-01-02 14:41:36 -0800

    Append further setup stuff

--- server/server.js
+++ server/server.js
@@ -9,8 +9,12 @@ const routes = require('./routes/api');
 
 require('dotenv').config();
 
+const path = require("path");
+
 const app = express(); // Initiate express.
 const port = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(cors({
     credentials: true,
@@ -25,3 +29,9 @@ app.use('/', routes);
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
+
+// This is there to fix the refresh issue. Without this, everytime the page is refreshed, the page will crash.
+// Got the solution from this article. https://ui.dev/react-router-cannot-get-url-refresh
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
