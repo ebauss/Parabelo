96d85145005cd199f38d1acc6e52ba07600dc14a
Evon Bausa <evon.bausa@gmail.com>
2023-01-05 23:56:42 -0800

    Revise text box and prompt to openai server

---
+++ client/src/components/ProductDescriptionComponent/ProductDescriptionComponent.js
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SendIcon from '@mui/icons-material/Send';
+import Typography from "@mui/material/Typography";
+
+const {Configuration, OpenAIApi} = require("openai");
+
+export default function ProductDescriptionComponent() {
+    /* Stores the string entered in the prompt text field. */
+    const [promptValue, setPromptValue] = React.useState('');
+
+    /* Stores the result string obtained from OpenAi. */
+    const [resultValue, setResultValue] = React.useState('');
+
+    /* Determines whether the loading animation is activated or not. */
+    const [loading, setLoading] = React.useState(false);
+
+    /**
+     * Handles the text changes in the text box.
+     *
+     * @param event contains data of the event
+     */
+    const handleChange = (event) => {
+        setPromptValue(event.target.value);
+    }
+
+    /**
+     * handles the generate button click.
+     *
+     * Send the prompt to the server; the server will then send the request to OpenAi.
+     */
+    const handleClick = async () => {
+        setLoading(true); // Starts the loading animation on the button.
+
+        const apiKeyResponse = await fetch("http://localhost:8000/getOpenAIApiKey",{
+            method: "Get",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+
+        const apiKeyData = await apiKeyResponse.text();
+
+        const configuration = new Configuration({
+            apiKey: apiKeyData,
+        });
+
+        const modifiedPrompt = 'Write a product description about ' + promptValue;
+
+        const openai = new OpenAIApi(configuration);
+
+        const aiApiResponse = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: modifiedPrompt,
+            temperature: 0.9,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+
+        const aiApiData = await aiApiResponse.data.choices[0].text;
+
+        if (aiApiData) {
+            setResultValue(aiApiData.trim());
+        }
+
+        setLoading(false); // Ends the loading animation on the button.
+    }
+
+    return (
+        <div>
+            <br/>
+            <Typography variant="h5" gutterBottom>
+                Product Description Writer
+            </Typography>
+            <br/>
+            <div>
+                <TextField id="outlined-basic"
+                           label="What product would you like me to write a description about?"
+                           variant="outlined"
+                           fullWidth
+                           onChange={handleChange}
+                           sx={{width: 600}}
+                />
+            </div>
+            <br/>
+            <div>
+                <LoadingButton
+                    size="small"
+                    onClick={handleClick}
+                    endIcon={<SendIcon />}
+                    loading={loading}
+                    loadingPosition="end"
+                    variant="contained"
+                >
+                    Go
+                </LoadingButton>
+            </div>
+            <br/>
+            <TextField
+                id="outlined-multiline-static"
+                label="Result"
+                multiline
+                rows={20}
+                placeholder="Your blog will appear here."
+                value={resultValue}
+                sx={{width: 600}}
+                InputLabelProps={{shrink: true}}
+                InputProps={{
+                    readOnly: true,
+                }}
+            />
+        </div>
+    )
+}
