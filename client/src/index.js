29ab60d85b3034bb5c28e5b4461389ba34b33234
Evon Bausa <evon.bausa@gmail.com>
2023-01-08 22:52:54 -0800

    Configure Auth0 provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <Auth0Provider
+                domain="dev-qw5gqp8rnisenpl3.us.auth0.com"
+                clientId="bukP5d3CKsrkfxfKlaWYN5AtifcCi522"
+                redirectUri={window.location.origin}
+            >
+                <App />
+            </Auth0Provider>
         </BrowserRouter>
     </React.StrictMode>
 );
