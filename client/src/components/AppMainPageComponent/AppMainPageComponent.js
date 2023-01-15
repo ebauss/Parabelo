a9e9f30fd7e4f7ec4ff55fc5813e26d61a6c095d
Evon Bausa <evon.bausa@gmail.com>
2023-01-15 14:19:56 -0800

    Append buttons without links

--- client/src/components/AppMainPageComponent/AppMainPageComponent.js
+++ client/src/components/AppMainPageComponent/AppMainPageComponent.js
@@ -1,11 +1,33 @@
 import * as React from 'react';
-
-import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
 
 export default function AppMainPageComponent() {
     return (
         <div>
-            <h1>Welcome to the app!</h1>
+            <br/>
+            <Typography variant="h5" gutterBottom>
+               Writing Tools
+            </Typography>
+            <br/>
+            <Grid container spacing={3}>
+                <Grid item xs={4}>
+                    <Button variant="outlined" size="large">Paraphrasing Tool</Button>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button variant="outlined" size="large">Blog Post Writer</Button>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button variant="outlined" size="large">Copy Writer</Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <Button variant="outlined" size="large">Product Description Writer</Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <Button variant="outlined" size="large">Email Marketing Writer</Button>
+                </Grid>
+            </Grid>
         </div>
     )
 }
