0ff1a29cce9d529736d7be5483eda3dbc835015a
Evon Bausa <evon.bausa@gmail.com>
2023-01-15 14:25:15 -0800

    Append links to buttons

--- client/src/components/AppMainPageComponent/AppMainPageComponent.js
+++ client/src/components/AppMainPageComponent/AppMainPageComponent.js
@@ -2,30 +2,43 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
 
 export default function AppMainPageComponent() {
     return (
         <div>
-            <br/>
+            <br />
             <Typography variant="h5" gutterBottom>
-               Writing Tools
+                Writing Tools
             </Typography>
-            <br/>
+            <br />
             <Grid container spacing={3}>
                 <Grid item xs={4}>
-                    <Button variant="outlined" size="large">Paraphrasing Tool</Button>
+                    <Link to='/app/paraphrasing' style={{ textDecoration: 'none', color: "black" }}>
+                        <Button variant="outlined" size="large">
+                            Paraphrasing Tool
+                        </Button>
+                    </Link>
                 </Grid>
                 <Grid item xs={4}>
-                    <Button variant="outlined" size="large">Blog Post Writer</Button>
+                    <Link to='/app/blogpost' style={{ textDecoration: 'none', color: "black" }}>
+                        <Button variant="outlined" size="large">Blog Post Writer</Button>
+                    </Link>
                 </Grid>
                 <Grid item xs={4}>
-                    <Button variant="outlined" size="large">Copy Writer</Button>
+                    <Link to='/app/copyWriter' style={{ textDecoration: 'none', color: "black" }}>
+                        <Button variant="outlined" size="large">Copy Writer</Button>
+                    </Link>
                 </Grid>
                 <Grid item xs={6}>
-                    <Button variant="outlined" size="large">Product Description Writer</Button>
+                    <Link to='/app/productDescriptionWriter' style={{ textDecoration: 'none', color: "black" }}>
+                        <Button variant="outlined" size="large">Product Description Writer</Button>
+                    </Link>
                 </Grid>
                 <Grid item xs={6}>
-                    <Button variant="outlined" size="large">Email Marketing Writer</Button>
+                    <Link to='/app/emailMarketingWriter' style={{ textDecoration: 'none', color: "black" }}>
+                        <Button variant="outlined" size="large">Email Marketing Writer</Button>
+                    </Link>
                 </Grid>
             </Grid>
         </div>
