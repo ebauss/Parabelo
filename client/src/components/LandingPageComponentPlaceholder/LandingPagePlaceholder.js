f78a8358b15cce07120974f43c1ad16580bb3253
Evon Bausa <evon.bausa@gmail.com>
2023-01-15 20:18:19 -0800

    Recompile build

--- client/src/components/LandingPageComponentPlaceholder/LandingPagePlaceholder.js
+++ client/src/components/LandingPageComponentPlaceholder/LandingPagePlaceholder.js
@@ -5,20 +5,9 @@ import {useAuth0} from "@auth0/auth0-react";
 export default function LandingPagePlaceholder() {
     const {loginWithRedirect} = useAuth0();
 
-    /**
-     * Handles the go-to web app click from the button.
-     */
-    const handleGoToWebAppClick = () => {
-        // Try using window.open() first. Maybe switch to Link later on?
-        window.open("/app", "_self");
-    }
-
     return (
         <div>
             <h1>Landing Page Placeholder</h1>
-            {/* <Button variant="outlined" size="large" onClick={handleGoToWebAppClick}>
-                Large
-            </Button> */}
             <Button variant="outlined" size="large" onClick={loginWithRedirect}>
                 Log In
             </Button>
--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,7 @@ root.render(
             <Auth0Provider
                 domain="dev-qw5gqp8rnisenpl3.us.auth0.com"
                 clientId="bukP5d3CKsrkfxfKlaWYN5AtifcCi522"
-                redirectUri="http://localhost:3000/app/"
+                redirectUri={window.location.origin + "/app/"}
             >
                 <App />
             </Auth0Provider>
