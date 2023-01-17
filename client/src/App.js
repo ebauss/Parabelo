cba2e61bee42911ad90df1be8a659d216466b8ac
Evon Bausa <evon.bausa@gmail.com>
2023-01-16 22:35:24 -0800

    Rename directory and files

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import LandingPagePlaceholder from "./components/LandingPageComponentPlaceholder/LandingPagePlaceholder";
 import WebApplication from "./pages/WebApplication/WebApplication";
 import {
     Routes,
     Route
 } from "react-router-dom";
+import LandingPageComponent from './components/LandingPageComponent/LandingPageComponent';
 
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="/app/*" element={<WebApplication/>}/>
-                <Route path="/" element={<LandingPagePlaceholder/>}/>
+                <Route path="/" element={<LandingPageComponent/>}/>
             </Routes>
         </div>
     );
