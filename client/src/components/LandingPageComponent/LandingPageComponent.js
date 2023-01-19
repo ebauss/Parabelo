49c8b29dcaa36fc9cab02a9604de2b2595bbba2a
Evon Bausa <evon.bausa@gmail.com>
2023-01-18 19:37:59 -0800

    Append hero image and text

--- client/src/components/LandingPageComponent/LandingPageComponent.js
+++ client/src/components/LandingPageComponent/LandingPageComponent.js
@@ -1,13 +1,32 @@
 import * as React from 'react';
-import {Button} from "@mui/material";
-import {useAuth0} from "@auth0/auth0-react";
+import Box from '@mui/material/Box';
+import { useAuth0 } from "@auth0/auth0-react";
+import landingPageHero from '../../assets/landing-page-hero.png';
+import { Typography } from '@mui/material';
 
 export default function LandingPageComponent() {
-    const {loginWithRedirect} = useAuth0(); // Need this for Call to Actions.
+    const { loginWithRedirect } = useAuth0(); // Need this for Call to Actions.
 
     return (
         <div>
-            <h1>Landing Page Placeholder</h1>
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                alignItems: 'center'
+            }}>
+                <Box
+                    component="img"
+                    src={landingPageHero}
+                    sx={{
+                        height: 600
+                    }}
+                />
+                <Typography variant="h2">
+                    Placeholder text here
+                </Typography>
+            </Box>
+
         </div>
     );
 }
