e64eb36b4583e45f6a9bd51955cb9c1070af81cc
Evon Bausa <evon.bausa@gmail.com>
2023-01-19 21:35:23 -0800

    Recompile front end with production environment links

--- client/src/components/EmailMarketingComponent/EmailMarketingComponent.js
+++ client/src/components/EmailMarketingComponent/EmailMarketingComponent.js
@@ -45,7 +45,7 @@ export default function EmailMarketingComponent() {
     const handleClick = async () => {
         setLoading(true); // Starts the loading animation on the button.
 
-        const apiKeyResponse = await fetch("http://localhost:8000/getOpenAIApiKey",{
+        const apiKeyResponse = await fetch("https://parabelo.herokuapp.com/getOpenAIApiKey",{
             method: "Get",
             credentials: "include",
             headers: {
