d41c3fd32f6c1bf8927463e5181085b9d60efcb2
Evon Bausa <evon.bausa@gmail.com>
2023-01-26 23:30:45 -0800

    Append basic setup

--- AppServer/Program.cs
+++ AppServer/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/getOpenAIApiKey", () => "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");
 
 app.Run();
