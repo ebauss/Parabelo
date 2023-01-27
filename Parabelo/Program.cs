dd753e981ffbf19c395700ecfb297c3d3d0e56c8
Evon Bausa <evon.bausa@gmail.com>
2023-01-26 22:12:59 -0800

    Basic setup of .Net. Still need to get CORS to work

--- Parabelo/Program.cs
+++ Parabelo/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-app.MapGet("/", () => "Hello World!");
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-app.Run();
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(name: MyAllowSpecificOrigins,
+                              policy =>
+                              {
+                                  policy.WithOrigins("http://localhost:3000");
+                              });
+        });
+
+        app.UseCors(MyAllowSpecificOrigins);
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.MapGet("/getOpenAIApiKey", () => "K9FMqbbEQR9RHsLnA48zT3BlbkFJf4KfIcqkMT0GBcXubtoq");
+
+        app.Run();
+    }
+}
