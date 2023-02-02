3a4f8a034b0a59da75918e55fc03419db2bd7f13
Evon Bausa <evon.bausa@gmail.com>
2023-02-01 21:04:47 -0800

    install mongoose

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes/api');
+// const mongoDBConnection = require('./database/connection');
 /* ------------------------------------ */
 
 require('dotenv').config();
