1fd122ebc275e16438065e1b74e19ed31dd3ae21
Evon Bausa <evon.bausa@gmail.com>
2023-02-01 21:31:39 -0800

    Update documents.js

--- server/models/documents.js
+++ server/models/documents.js
@@ -4,10 +4,12 @@ const Schema = mongoose.Schema;
 // Create schema for documents
 const documentSchema = new Schema({
     type: String,
-    owner: String, // This is the id that "owns" this document.
+    owner: String, // This is the id that "owns" this document. Use this like a foreign key
     prompt: String,
     thingsToMention: String,
-    contents: String
+    contents: String,
+    writingStyle: String,
+    tone: String
 })
 
 const documentModel = mongoose.model("documents", documentSchema);
