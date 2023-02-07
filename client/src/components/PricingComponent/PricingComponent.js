9ae1fed27e54f7c9da7376fbe9006c4c655674f8
Evon Bausa <evon.bausa@gmail.com>
2023-02-06 23:33:13 -0800

    Add reusable component for card

--- client/src/components/PricingComponent/PricingComponent.js
+++ client/src/components/PricingComponent/PricingComponent.js
@@ -1,9 +1,7 @@
 import { List, ListItem, ListItemAvatar, ListItemText, Paper, Typography } from '@mui/material';
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import CheckIcon from '@mui/icons-material/Check';
+import PricingCard from './PricingCard';
 
 export default function PricingComponent() {
     return (
@@ -16,256 +14,37 @@ export default function PricingComponent() {
                 paddingRight: 10
             }}>
                 <Grid item xs={12} sm={12} md={12} lg={4}>
-                    <Paper elevation={3} sx={{
-                        paddingTop: 2,
-                        paddingBottom: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center'
-                    }}>
-                        <Typography variant="h4"
-                            sx={{
-                                fontWeight: 'bold'
-                            }}>
-                            Basic
-                        </Typography>
-                        <br />
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Typography variant="h2" sx={{ fontWeight: 'bold' }}>
-                                $9.99
-                            </Typography>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'flex-start',
-                                    justifyContent: 'center',
-                                    marginLeft: 1
-                                }}>
-                                <Typography variant="h6" sx={{ lineHeight: 1 }}>
-                                    Per
-                                </Typography>
-                                <Typography variant="h6" sx={{ lineHeight: 1 }}>
-                                    Month
-                                </Typography>
-                            </Box>
-                        </Box>
-                        <br />
-                        <Button
-                            variant="contained"
-                            size="large"
-                            color="secondary"
-                        >
-                            Subscribe
-                        </Button>
-                        <br />
-                        <br />
-                        <Box>
-                            <Typography variant="h6">
-                                This Includes:
-                            </Typography>
-                            <List dense={true} >
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="Feature 1" />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="Test feature" />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="Some other feature" />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="A random feature" />
-                                </ListItem>
-                            </List>
-                        </Box>
-                    </Paper>
+                    <PricingCard 
+                    productName="Regular"
+                    productPrice="9.99"
+                    ctaText="Subscribe"
+                    featureOne="First feature"
+                    featureTwo="Some random fun feature"
+                    featureThree="Another feature"
+                    featureFour="The best feature" 
+                    />
                 </Grid>
                 <Grid item xs={12} sm={12} md={12} lg={4}>
-                    <Paper elevation={3} sx={{
-                        paddingTop: 2,
-                        paddingBottom: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center'
-                    }}>
-                        <Typography variant="h4"
-                            sx={{
-                                fontWeight: 'bold'
-                            }}>
-                            Plus
-                        </Typography>
-                        <br />
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Typography variant="h2" sx={{ fontWeight: 'bold' }}>
-                                $29.99
-                            </Typography>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'flex-start',
-                                    justifyContent: 'center',
-                                    marginLeft: 1
-                                }}>
-                                <Typography variant="h6" sx={{ lineHeight: 1 }}>
-                                    Per
-                                </Typography>
-                                <Typography variant="h6" sx={{ lineHeight: 1 }}>
-                                    Month
-                                </Typography>
-                            </Box>
-                        </Box>
-                        <br />
-                        <Button
-                            variant="contained"
-                            size="large"
-                            color="secondary"
-                        >
-                            Subscribe
-                        </Button>
-                        <br />
-                        <br />
-                        <Box>
-                            <Typography variant="h6">
-                                This Includes:
-                            </Typography>
-                            <List dense={true} >
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="Feature 1" />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="Test feature" />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="Some other feature" />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="A random feature" />
-                                </ListItem>
-                            </List>
-                        </Box>
-                    </Paper>
+                    <PricingCard 
+                    productName="Plus"
+                    productPrice="29.99"
+                    ctaText="Subscribe"
+                    featureOne="First feature"
+                    featureTwo="Some random fun feature"
+                    featureThree="Another feature"
+                    featureFour="The best feature" 
+                    />
                 </Grid>
                 <Grid item xs={12} sm={12} md={12} lg={4}>
-                    <Paper elevation={3} sx={{
-                        paddingTop: 2,
-                        paddingBottom: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center'
-                    }}>
-                        <Typography variant="h4"
-                            sx={{
-                                fontWeight: 'bold'
-                            }}>
-                            Premium
-                        </Typography>
-                        <br />
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Typography variant="h2" sx={{ fontWeight: 'bold' }}>
-                                $49.99
-                            </Typography>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'flex-start',
-                                    justifyContent: 'center',
-                                    marginLeft: 1
-                                }}>
-                                <Typography variant="h6" sx={{ lineHeight: 1 }}>
-                                    Per
-                                </Typography>
-                                <Typography variant="h6" sx={{ lineHeight: 1 }}>
-                                    Month
-                                </Typography>
-                            </Box>
-                        </Box>
-                        <br />
-                        <Button
-                            variant="contained"
-                            size="large"
-                            color="secondary"
-                        >
-                            Subscribe
-                        </Button>
-                        <br />
-                        <br />
-                        <Box>
-                            <Typography variant="h6">
-                                This Includes:
-                            </Typography>
-                            <List dense={true} >
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="Feature 1" />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="Test feature" />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="Some other feature" />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemAvatar>
-                                        <CheckIcon />
-                                    </ListItemAvatar>
-                                    <ListItemText primary="A random feature" />
-                                </ListItem>
-                            </List>
-                        </Box>
-                    </Paper>
+                    <PricingCard 
+                    productName="Premium"
+                    productPrice="49.99"
+                    ctaText="Subscribe"
+                    featureOne="First feature"
+                    featureTwo="Some random fun feature"
+                    featureThree="Another feature"
+                    featureFour="The best feature" 
+                    />
                 </Grid>
             </Grid>
         </div>
