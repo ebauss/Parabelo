fafe2d39763266148153fac12ebe327e33e3ef87
Evon Bausa <evon.bausa@gmail.com>
2023-02-27 21:31:39 -0800

    Append free trial test component

---
+++ client/src/components/FreeTrialComponent/FreeTrialPostSignUp.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Typography from "@mui/material/Typography";
+
+export default function FreeTrialPostSignUp() {
+    return (
+        <div>
+            <Typography variant="h1" gutterBottom>
+                Free Trial Test
+            </Typography>
+        </div>
+    )
+}
