c7c6c9c1968eadb8833777418cbd5970cfe2f626
Evon Bausa <evon.bausa@gmail.com>
2023-03-01 22:10:57 -0800

    Update PricingComponentPostSignUp.js

--- client/src/components/PricingComponent/PricingComponentPostSignUp.js
+++ client/src/components/PricingComponent/PricingComponentPostSignUp.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import PricingCard from './PricingCard';
 
 export default function PricingComponentPostSignUp() {
     return (
