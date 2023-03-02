f4f3f11b8fe20488b5530764a4f40afbd2891d9c
Evon Bausa <evon.bausa@gmail.com>
2023-03-01 22:07:29 -0800

    Update PricingComponentPostSignUp.js

--- client/src/components/PricingComponent/PricingComponentPostSignUp.js
+++ client/src/components/PricingComponent/PricingComponentPostSignUp.js
@@ -7,6 +7,7 @@ export default function PricingComponentPostSignUp() {
         <div>
             <br />
             <br />
+            {/* The pricing table here can be updated on Stripe. */}
             <stripe-pricing-table pricing-table-id="prctbl_1MXIgABEpnOVMkQ6PsmYO05R"
                 publishable-key="pk_test_51MXHVMBEpnOVMkQ6UrWxPzQR8ZYR6gQI6M8LBYOZwGwcmSrwoKOqnXPJP2s27xHRX4lFbEr3Hpk836nx2l1IL5wA00r5XyEqfj">
             </stripe-pricing-table>
