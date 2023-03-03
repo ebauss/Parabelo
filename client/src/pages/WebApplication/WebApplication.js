04f9e8a3fe713b107991deeabe794f94c8097014
Evon Bausa <evon.bausa@gmail.com>
2023-03-02 21:24:46 -0800

    Add pricing to login flow

--- client/src/pages/WebApplication/WebApplication.js
+++ client/src/pages/WebApplication/WebApplication.js
@@ -17,7 +17,7 @@ import PricingComponentPostSignUp from '../../components/PricingComponent/Pricin
 
 export default function WebApplication() {
     const { user, isAuthenticated, isLoading } = useAuth0();
-    
+
     if (isLoading) {
         return (
             <div>
@@ -27,21 +27,31 @@ export default function WebApplication() {
     }
 
     if (isAuthenticated) {
-        return (
-            <div>
-                <NavbarWebApp></NavbarWebApp>
-                <Routes>
-                    <Route path="/blogpost" element={<BlogPostComponent userDetails={user} />} />
-                    <Route path="/copyWriter" element={<CopyWriter userDetails={user} />} />
-                    <Route path="/emailMarketingWriter" element={<EmailMarketingComponent userDetails={user} />} />
-                    <Route path="/freeTrial" element={<FreeTrialPostSignUp userDetails={user} />} />
-                    <Route path="/paraphrasing" element={<ParaphrasingComponent userDetails={user} />} />
-                    <Route path="/pricingPostSignUp" element={<PricingComponentPostSignUp userDetails={user} />} />
-                    <Route path="/productDescriptionWriter" element={<ProductDescriptionComponent userDetails={user} />} />
-                    <Route path="/" element={<AppMainPageComponent />} />
-                </Routes>
-            </div>
-        )
+        if (user.userNeedsSubscription) {
+            return (
+                <div>
+                    <NavbarWebApp></NavbarWebApp>
+                    <PricingComponentPostSignUp />
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <NavbarWebApp></NavbarWebApp>
+                    <Routes>
+                        <Route path="/blogpost" element={<BlogPostComponent userDetails={user} />} />
+                        <Route path="/copyWriter" element={<CopyWriter userDetails={user} />} />
+                        <Route path="/emailMarketingWriter" element={<EmailMarketingComponent userDetails={user} />} />
+                        <Route path="/freeTrial" element={<FreeTrialPostSignUp userDetails={user} />} />
+                        <Route path="/paraphrasing" element={<ParaphrasingComponent userDetails={user} />} />
+                        <Route path="/pricingPostSignUp" element={<PricingComponentPostSignUp userDetails={user} />} />
+                        <Route path="/productDescriptionWriter" element={<ProductDescriptionComponent userDetails={user} />} />
+                        <Route path="/" element={<AppMainPageComponent />} />
+                    </Routes>
+                </div>
+            )
+        }
+
     } else {
         return (
             <Navigate to="/" />
