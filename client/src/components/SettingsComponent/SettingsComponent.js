121267ebfa49d1772aecdd22a921e2564ddd84d1
Evon Bausa <evon.bausa@gmail.com>
2023-03-08 21:41:12 -0800

    Update SettingsComponent.js

--- client/src/components/SettingsComponent/SettingsComponent.js
+++ client/src/components/SettingsComponent/SettingsComponent.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
 
 export default function SettingsComponent(props) {
@@ -8,6 +8,9 @@ export default function SettingsComponent(props) {
             <Typography variant="h5">
                 Settings
             </Typography>
+            <br />
+            <Button variant="outlined">Manage Subscription</Button>
+            <br />
         </div>
     )
 }
