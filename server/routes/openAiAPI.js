1443af6a62cc0a3ebf78d18ebec94d1ef5c4b9a9
Evon Bausa <evon.bausa@gmail.com>
2023-03-16 22:19:46 -0700

    Create openAiAPI.js

---
+++ server/routes/openAiAPI.js
@@ -0,0 +1,48 @@
+/**
+ * Require node dependencies.
+ */
+const express = require('express');
+const router = express.Router();
+/* ------------------------------------ */
+
+router.post('/requestTextResponse', async (req, res) => {
+    const { Configuration, OpenAIApi } = require("openai");
+
+    const prompt = req.body.prompt;
+    const temperature = req.body.temperature;
+    const max_tokens = req.body.max_tokens;
+    const top_p = req.body.top_p;
+    const frequency_penalty = req.body.frequency_penalty;
+    const presence_penalty = req.body.presence_penalty;
+
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    const moderationResponse = await openai.createModeration({
+        input: prompt
+    })
+
+    // Check if prompt follows OpenAi usage policies using the OpenAi moderation endpoint.
+    const isPromptFlagged = await moderationResponse.data.results[0].flagged;
+
+    if (!isPromptFlagged) {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            temperature: temperature,
+            max_tokens: max_tokens,
+            top_p: top_p,
+            frequency_penalty: frequency_penalty,
+            presence_penalty: presence_penalty,
+        });
+        console.log(response);
+    } else {
+        res.send("Prompt is flagged");
+    }
+})
+
+
+module.exports = router;
