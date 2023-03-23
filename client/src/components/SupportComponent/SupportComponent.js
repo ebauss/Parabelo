b12ceebbf0329bf28a8425f9ccbfd430160e4269
Evon Bausa <evon.bausa@gmail.com>
2023-03-22 20:21:51 -0700

    Add inner functions

--- client/src/components/SupportComponent/SupportComponent.js
+++ client/src/components/SupportComponent/SupportComponent.js
@@ -3,8 +3,29 @@ import { Box } from '@mui/system';
 import React from 'react';
 import TextField from '@mui/material/TextField';
 
-export default function SupportComponent() {
-    const handleChange = (event) => {
+export default function SupportComponent(props) {
+    const [firstNameValue, setFirstNameValue] = React.useState(props.userDetails ? props.userDetails.given_name : '');
+    const [lastNameValue, setLastNameValue] = React.useState(props.userDetails ? props.userDetails.first_name : '');
+    const [emailValue, setEmailValue] = React.useState(props.userDetails ? props.userDetails.email : '');
+    const [emailBodyValue, setEmailBodyValue] = React.useState('');
+
+    const handleFirstNameChange = (event) => {
+        setFirstNameValue(event.target.value);
+    }
+
+    const handleLastNameChange = (event) => {
+        setLastNameValue(event.target.value);
+    }
+
+    const handleEmailChange = (event) => {
+        setEmailValue(event.target.value);
+    }
+
+    const handleEmailBodyChange = (event) => {
+        setEmailBodyValue(event.target.value);
+    }
+
+    const sendEmail = async () => {
 
     }
 
@@ -22,7 +43,7 @@ export default function SupportComponent() {
                 Please send us an email via this form if you have any questions.
             </Typography>
             <br />
-            <Button variant="contained">
+            <Button variant="contained" onChange={sendEmail} >
                 Send Email
             </Button>
             <br />
@@ -30,30 +51,37 @@ export default function SupportComponent() {
                 <TextField id="outlined-basic"
                     label="First Name"
                     variant="outlined"
-                    onChange={handleChange}
+                    onChange={handleFirstNameChange}
                     sx={{ width: 300, margin: 2 }}
+                    inputProps={{ maxLength: 255 }}
                 />
                 <TextField id="outlined-basic"
                     label="Last Name"
                     variant="outlined"
-                    onChange={handleChange}
+                    onChange={handleLastNameChange}
                     sx={{ width: 300, margin: 2 }}
+                    inputProps={{ maxLength: 255 }}
                 />
             </Box>
             <TextField id="outlined-basic"
                 label="Email"
                 variant="outlined"
-                onChange={handleChange}
-                sx={{ width: 632, margin: 2 }}
+                onChange={handleEmailChange}
+                sx={{ width: {
+                    sm: 632
+                }, 
+                margin: 2 }}
+                inputProps={{ maxLength: 255 }}
             />
             <br />
             <TextField id="outlined-basic"
                 label="Email Body"
                 variant="outlined"
-                onChange={handleChange}
+                onChange={handleEmailBodyChange}
                 multiline
                 rows={20}
                 sx={{ width: 632, margin: 2 }}
+                inputProps={{ maxLength: 300000 }}
             />
         </Box>
     )
