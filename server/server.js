2f53fd58f752839e8d05905b9b95cc380f343f2d
Evon Bausa <evon.bausa@gmail.com>
2023-03-29 22:05:57 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -49,6 +49,12 @@ mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 // api URL is https://<domain>/<route>. When running locally, it is http://localhost:<port>/<route>
 app.use('/', routes);
 
