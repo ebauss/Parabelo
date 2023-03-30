6f7c8823564324b29f6efc6d9c1585c0a9952809
Evon Bausa <evon.bausa@gmail.com>
2023-03-29 20:27:00 -0700

    change links and stuff

--- client/src/index.js
+++ client/src/index.js
@@ -25,9 +25,9 @@ root.render(
             <BrowserRouter>
                 {/* TODO: Add these files to a .env so that they are secret. */}
                 <Auth0Provider
-                    domain="parabelo-staging.us.auth0.com"
-                    clientId="u08av9Obvtx6tR4sskAj7UjHFz4hmwlw"
-                    redirectUri="https://parabelo-staging.netlify.app/app/"
+                    domain="dev-qw5gqp8rnisenpl3.us.auth0.com"
+                    clientId="bukP5d3CKsrkfxfKlaWYN5AtifcCi522"
+                    redirectUri="https://parabelo.netlify.app/app/"
 
                     // Ensures that it works for Safari with their ITP stuff. Before when refreshing on a logged in user, it will redirect back to the landing page.
                     cacheLocation="localstorage" 
