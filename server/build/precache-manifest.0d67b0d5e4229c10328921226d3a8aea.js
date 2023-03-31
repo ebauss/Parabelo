d5b84f760aa6c36509e346ba6ca649f9e7d1671b
Evon Bausa <evon.bausa@gmail.com>
2023-03-30 19:32:59 -0700

    add it

---
+++ server/build/precache-manifest.0d67b0d5e4229c10328921226d3a8aea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6a0887175e8fc10d7e8",
+    "url": "/static/css/main.0b78a858.chunk.css"
+  },
+  {
+    "revision": "e6a0887175e8fc10d7e8",
+    "url": "/static/js/main.e6a08871.chunk.js"
+  },
+  {
+    "revision": "c375fc145b8def53aacc",
+    "url": "/static/js/1.c375fc14.chunk.js"
+  },
+  {
+    "revision": "5b12d224ac17599b8d8d",
+    "url": "/static/js/2.5b12d224.chunk.js"
+  },
+  {
+    "revision": "ef3a35d654bb23cd7494",
+    "url": "/static/js/runtime~main.ef3a35d6.js"
+  },
+  {
+    "revision": "51d807602cf9ba3b10e95fb46f3b55c6",
+    "url": "/static/media/heart-graphic.51d80760.png"
+  },
+  {
+    "revision": "b3ba733d92fbb6066feec57590b3345b",
+    "url": "/static/media/happy-landing-page-hero-no-background.b3ba733d.png"
+  },
+  {
+    "revision": "e7f0c5d0c8cea7ac8ddbb13a15ff91c3",
+    "url": "/index.html"
+  }
+];
