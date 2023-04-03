1231d178fb0647d173148a4ac962eb8c4091aeea
Evon Bausa <evon.bausa@gmail.com>
2023-04-03 12:18:26 -0700

    Update HistoryComponent.js

--- client/src/components/HistoryComponent/HistoryComponent.js
+++ client/src/components/HistoryComponent/HistoryComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Paper } from "@mui/material";
 
 export default function HistoryComponent() {
     return (
@@ -9,6 +9,23 @@ export default function HistoryComponent() {
                 History
             </Typography>
             <br />
+            <Paper elevation={3} sx={{
+                "&:hover": {
+                    transform: "scale3d(1.05, 1.05, 1.05)",
+                },
+                height: "100%"
+            }}>
+                <br />
+                <Typography variant="h5"
+                    sx={{
+                        paddingLeft: 4,
+                        paddingRight: 4
+                    }}
+                >
+                    
+                </Typography>
+                <br />
+            </Paper>
         </Box>
     )
 }
