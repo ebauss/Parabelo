9d3f1e22d34234dd852f3a38d75609ba9dea8b08
Evon Bausa <evon.bausa@gmail.com>
2023-04-03 12:08:06 -0700

    Add History page

---
+++ client/src/components/HistoryComponent/HistoryComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Typography } from "@mui/material";
+
+export default function HistoryComponent() {
+    return (
+        <Box>
+            <br />
+            <Typography variant="h5" gutterBottom>
+                History
+            </Typography>
+            <br />
+        </Box>
+    )
+}
